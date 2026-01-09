77ca45851c3e9852eed6a56e1d63b3fc8b222558
vicky1013kr@gmail.com <kangb1@my.smccd.edugit config --global user.name>
2026-01-09 13:45:03 +0900

    css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import { createRoot } from "react-dom/client";
+import React from 'react';
+import App from "./app/App";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
+// index.css를 거치지 않고 직접 불러오기 (에러 방지)
+import "./styles/fonts.css";
+import "./styles/tailwind.css";
+import "./styles/theme.css";
+import "./styles/index.css"; // 이건 혹시 다른 코드가 들어있을지 모르니 유지
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
